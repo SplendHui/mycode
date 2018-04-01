b8caa4f744a248d9bd5177bc74a4e8906eb9f226
503ubuntu <752406912@qq.com>
2018-04-01 10:15:01 +0800

    ubuuntu503

--- code/C++/associative_container.cpp
+++ code/C++/associative_container.cpp
@@ -11,7 +11,6 @@ int main()
         string word;
         while (cin >> word)
             ++word_count[word];
-
         for (const auto &w : word_count)
             cout << w.first << " occurs " << w.second << ((w.second > 1) ? " times " : " time ") << endl;
     }
