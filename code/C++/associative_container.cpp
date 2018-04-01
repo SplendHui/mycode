3070e267412cf488c0fbce6684a86e85e52387e8
503ubuntu <752406912@qq.com>
2018-04-01 10:13:01 +0800

    ubuuntu503

--- code/C++/associative_container.cpp
+++ code/C++/associative_container.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <set>
 #include <map>
+#include <string>
 using namespace std;
 int main()
 {
+    // map
+    {
+        map<string, size_t> word_count;
+        string word;
+        while (cin >> word)
+            ++word_count[word];
+    }
+
     return 0;
 }
