0618a8539b169f33eb7074cd1f6e20102be2d8ae
503ubuntu <752406912@qq.com>
2018-04-01 10:20:01 +0800

    ubuuntu503

--- code/C++/associative_container.cpp
+++ code/C++/associative_container.cpp
@@ -9,17 +9,19 @@ int main()
     {
         map<string, size_t> word_count;
         string word;
-        while (cin >> word)
-            ++word_count[word];
+        //while (cin >> word)
+        //    ++word_count[word];
         for (const auto &w : word_count)
             cout << w.first << " occurs " << w.second << ((w.second > 1) ? " times " : " time ") << endl;
     }
+
     {
         map<string, size_t> word_count;
         set<string> exclude = {"The", "But", "And", "Or", "An", "A", "the", "but", "and", "or", "an", "a"};
         string word;
         while (cin >> word)
-            ++word_count[word];
+            if (exclude.find(word) == exclude.end())
+                ++word_count[word];
         for (const auto &w : word_count)
             cout << w.first << " occurs " << w.second << ((w.second > 1) ? " times " : " time ") << endl;
     }
