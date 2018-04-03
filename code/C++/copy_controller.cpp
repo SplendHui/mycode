72b739f1fe760960b4ae9f0f6f10fa543d47acc5
splend <xielehui@qq.com>
2018-04-03 18:07:29 +0800

    ok

--- code/C++/copy_controller.cpp
+++ code/C++/copy_controller.cpp
@@ -39,12 +39,31 @@ struct NoDtor
     NoDtor() = default;
     ~NoDtor() = delete;
 };
+class HasPtr
+{
+  public:
+    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+    HasPtr(const HasPtr &p) : ps(new std::string(*p.ps)), i(p.i) {}
+    HasPtr &operator=(const HasPtr &);
+    ~HasPtr() { delete ps; }
+
+  private:
+    std::string *ps;
+    int i;
+};
+HasPtr &HasPtr::operator=(const HasPtr &rhs)
+{
+    auto newp = new string(*rhs.ps);
+    delete ps;
+    ps = newp;
+    return *this;
+}
 int main()
 {
     /*
     {
         NoDtor nd; // error 
-        NoDtor *p = new NoDtor();
+        NoDtor *p = new NoDtor(); it can do, but can't free memory;
         delete p; //error
     }
     */
