09e4d50e45f2b3caa14315502fbf60a377da93cd
splend <xielehui@qq.com>
2018-04-06 10:29:01 +0800

    ok

--- code/C++/test.cpp
+++ code/C++/test.cpp
@@ -24,7 +24,7 @@ int main()
 
     vector<A> v1;
     set<A *> v2;
-    for (int i = 0; i < 3000000; i++)
+    for (int i = 0; i < 10000000; i++)
     {
         A *a = new A();
         v2.insert(v2.begin(), a);
@@ -32,7 +32,7 @@ int main()
     // cout << "----------------------------" << endl;
     // cout << v2.size() << endl;
     //auto v2c = std::move(v2);
-    for (auto f : v2)
+    for (auto f : std::move(v2))
     {
         // cout << " nice " << endl;
     }
