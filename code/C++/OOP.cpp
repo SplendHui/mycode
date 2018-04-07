9122c7d899447fa5949db0f74789f5564df1e31b
splend <xielehui@qq.com>
2018-04-07 20:14:01 +0800

    ok

--- code/C++/OOP.cpp
+++ code/C++/OOP.cpp
@@ -27,9 +27,9 @@ class Bulk_quote : public Quote
   public:
     Bulk_quote() = default;
     Bulk_quote::Bulk_quote(const std::string &book, double p, std::size_t qty, double disc) : Quote(book, p), min_qty(qty), discount(disc) {}
-    double net_price(std::size_t) const override
+    double net_price(std::size_t cnt)
     {
-        if (cn >= min_qty)
+        if (cnt >= min_qty)
             return cnt * (1 - discount) * price;
         else
             return cnt * price;
