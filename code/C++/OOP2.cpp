de61cb4170c64758a664837f7c4ca4dc0f2dddfe
splend <xielehui@qq.com>
2018-04-08 15:18:01 +0800

    ok

--- code/C++/OOP2.cpp
+++ code/C++/OOP2.cpp
@@ -54,7 +54,7 @@ class Derived_from_Public : public Pub_Derv
     {
         return prot_mem;
     }
-    ~Derived_from_Public() {}
+    ~Derived_from_Public() { cout << "Derived_from_Public destroy" << endl; }
 };
 
 class Derived_from_Private : public Priv_Derv
