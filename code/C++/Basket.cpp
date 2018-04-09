812488a486c3f1374cff12d0b8a07b9d2e5a706d
splend <xielehui@qq.com>
2018-04-09 15:00:01 +0800

    ok

--- code/C++/Basket.cpp
+++ code/C++/Basket.cpp
@@ -6,7 +6,10 @@
 class Basket
 {
   public:
-    void add_item(const std::shared_ptr<Quote> &sale) {}
+    void add_item(const std::shared_ptr<Quote> &sale)
+    {
+        items.insert(sale);
+    }
     double total_receipt(std::ostream &) const;
 
   private:
@@ -18,8 +21,27 @@ class Basket
     std::multiset<std::shared_ptr<Quote>, decltype(compare) *> items{compare};
 };
 
+double Basket::total_receipt(ostream &os) const
+{
+    double sum = 0.0;
+
+    for (auto iter = items.cbegin();
+         iter != items.cend();
+         iter = items.upper_bound(*iter))
+    {
+        sum += print_total(os, **iter, items.count(*iter));
+    }
+
+    os << "Total Sale:" << sum << endl;
+
+    return sum;
+}
+
 int main()
 {
+    Basket bsk;
+    bsk.add_item(make_shared<Quote>("123", 45));
+    bsk.add_item(make_shared<Bulk_quote>("345", 45, 3, .15));
 
     return 0;
 }
