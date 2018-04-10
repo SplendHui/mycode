6f1d0da19f33045cc321d5b1f508627f7a39438e
503ubuntu <752406912@qq.com>
2018-04-10 17:09:03 +0800

    ubuuntu503

--- code/C++/main.cpp
+++ code/C++/main.cpp
@@ -1,14 +1,14 @@
+#include <iostream>
 #include "textquery.h"
+#include "textquery.cpp"
 
 int main(int argc, char *argv[])
 {
     std::ifstream is(argv[1]);
     TextQuery t1(is);
     std::string word;
-    Query q = Query("fiery") & Query("bird") | Query("wind");
+    Query q = Query("fiery") | Query("bird") | Query("wind");
     std::cout << q << std::endl;
     std::cout << q.eval(t1) << std::endl;
-
-    std::cin.get();
     return 0;
 }
