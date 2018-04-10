7c2f9cc504885a46127431c44ca4daef9fc8cf9b
splend <xielehui@qq.com>
2018-04-10 09:01:10 +0800

    ok

--- code/C++/Basket.cpp
+++ code/C++/Basket.cpp
@@ -11,12 +11,25 @@ class Basket
         items.insert(sale);
     }
     double total_receipt(std::ostream &) const;
+    void print() const
+    {
+        for (auto i : items)
+        {
+            cout << "isbn = " << i->isbn() << " price = " << i->net_price(1) << endl;
+        }
+        auto be = items.begin();
+        be++;
+        auto up1 = items.upper_bound(*be);
+        cout << "!isbn = " << (*up1)->isbn() << " price = " << (*up1)->net_price(1) << endl;
+        up1 = items.lower_bound(*be);
+        cout << "!isbn = " << (*up1)->isbn() << " price = " << (*up1)->net_price(1) << endl;
+    }
 
   private:
     static bool compare(const std::shared_ptr<Quote> &lhs,
                         const std::shared_ptr<Quote> &rhs)
     {
-        return lhs->isbn() < rhs->isbn();
+        return lhs->isbn() > rhs->isbn();
     }
     std::multiset<std::shared_ptr<Quote>, decltype(compare) *> items{compare};
 };
@@ -41,7 +54,12 @@ int main()
 {
     Basket bsk;
     bsk.add_item(make_shared<Quote>("123", 45));
-    bsk.add_item(make_shared<Bulk_quote>("345", 45, 3, .15));
+    bsk.add_item(make_shared<Bulk_quote>("345", 1, 3, .15));
+    // bsk.add_item(make_shared<Bulk_quote>("345", 2, 3, .15));
+    // bsk.add_item(make_shared<Bulk_quote>("345", 3, 3, .15));
+    // bsk.add_item(make_shared<Bulk_quote>("3456", 4, 3, .15));
+    // bsk.add_item(make_shared<Bulk_quote>("345", 5, 3, .15));
+    // bsk.print();
 
     return 0;
 }
