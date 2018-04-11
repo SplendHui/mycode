ad56f31afd263715b71db0ea9191834d0171232b
splend <xielehui@qq.com>
2018-04-11 08:45:51 +0800

    ok

--- code/C++/textquery_main.cpp
+++ code/C++/textquery_main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     std::ifstream is(argv[1]);
     TextQuery t1(is);
     std::string word;
-    Query q = Query("fiery") | Query("bird") | Query("wind");
+    Query q = (Query("fiery") & Query("bird")) | Query("wind");
     std::cout << q << std::endl;
     std::cout << q.eval(t1) << std::endl;
     return 0;
