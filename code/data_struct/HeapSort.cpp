23c3759103a0c1afc4e1f89e00bbf993aa4f7316
splend <xielehui@qq.com>
2018-05-14 08:57:23 +0800

    ok

---
+++ code/data_struct/HeapSort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+typedef int keytype;
+
+void adjust(keytype k[], int i, int n)
+{
+    int j;
+    keytype tmp;
+    tmp = k[i];
+    j = i * 2;
+
+    while (j <= n)
+    {
+        if (j < n && k[j] < k[j + 1])
+        {
+            j++;
+        }
+        if (tmp >= k[j])
+        {
+            break;
+        }
+        k[j / 2] = k[j];
+        j = j * 2;
+    }
+    k[j / 2] = tmp;
+}
+
+void HeapSort(keytype k[], int n)
+{
+    for (int i = n / 2; i >= 1; i--)
+    {
+        adjust(k, i, n);
+    }
+    int tmp;
+    for (int i = n - 1; i >= 1; i--)
+    {
+        tmp = k[i + 1];
+        k[i + 1] = k[1];
+        k[1] = tmp;
+        adjust(k, 1, i);
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    int array[21];
+    for (int i = 0; i < 21; i++)
+    {
+        array[i] = rand() % 1000;
+    }
+
+    HeapSort(array, 20);
+
+    for (int i = 1; i < 21; i++)
+        cout << array[i] << " ";
+    cout << endl;
+}
